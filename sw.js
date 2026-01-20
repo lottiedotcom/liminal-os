137d5e69b19146bc22936f48cbf0877e5ab8dfa1
lottiedotcom <notilottie@gmail.com>
2026-01-20 16:52:05 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,49 +1,5 @@
-// VERSION v4 - Adds Mood Tints & Visual Updates
-const CACHE_NAME = 'inbetween-v4';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.png'
-];
-
-// Install Event
-self.addEventListener('install', (e) => {
-  self.skipWaiting(); 
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
-});
-
-// Activate Event
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  return self.clients.claim();
-});
-
-// Fetch Event
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
-});
-
-
-
-// VERSION v6 - Corrected Time Names
-const CACHE_NAME = 'inbetween-v6';
+// VERSION v7 - Added Rapid and Glacial
+const CACHE_NAME = 'inbetween-v7';
 const ASSETS = [
   './',
   './index.html',
