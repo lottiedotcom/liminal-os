9473dfa9bf257e66dd2fc0878ee3cd79b220a01f
lottiedotcom <notilottie@gmail.com>
2026-01-20 16:31:06 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -39,3 +39,44 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
+
+
+
+// VERSION v5 - Broken Clock Logic
+const CACHE_NAME = 'inbetween-v5';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting(); 
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
