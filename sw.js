e3c55eb12cd533d5220772a6a59299153a08c40f
lottiedotcom <notilottie@gmail.com>
2026-01-20 22:09:00 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-// VERSION v14 - Tuner Mechanic Update
-const CACHE_NAME = 'inbetween-v14';
+const CACHE_NAME = 'inbetween-v2.1'; // Incrementing version to force reload
 const ASSETS = [
   './',
   './index.html',
@@ -7,32 +6,29 @@ const ASSETS = [
   './icon.png'
 ];
 
-self.addEventListener('install', (e) => {
-  self.skipWaiting(); 
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ASSETS);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }));
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
     })
   );
-  return self.clients.claim();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
