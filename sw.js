76bc7df022ae5cb39aa2260250f8ef981926cf4b
lottiedotcom <notilottie@gmail.com>
2026-01-21 08:46:06 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'inbetween-v5.5-NO-CLICKS';
+const CACHE_NAME = 'inbetween-v6.0-SILENT'; // Version bump forces new download
 const ASSETS = [
   './',
   './index.html',
@@ -6,15 +6,17 @@ const ASSETS = [
   './icon.png'
 ];
 
+// 1. INSTALL: Caches the new silent files
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ASSETS);
     })
   );
-  self.skipWaiting();
+  self.skipWaiting(); // Forces this new SW to activate immediately
 });
 
+// 2. ACTIVATE: Deletes the old version (v5.x, v4.x, etc)
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => {
@@ -25,6 +27,7 @@ self.addEventListener('activate', event => {
   );
 });
 
+// 3. FETCH: Serve from cache
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
